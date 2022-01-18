6a6a6900419be8583d1e4491d52a0fd187b1e735
Matheus <matheus.jimenez@hotmail.com.br>
2022-01-17 21:57:11 -0300

    Fixed minor bugs on middleware within sucessfull tests

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  if(!username)
+    return response.status(405).json({ error: "Invalid Username"});
+
+  const user = users.find((user) => user.username === username);
+  
+  if(!user)
+    return response.status(404).json({ error: "Invalid Username!" });
+
+  request.user = user;
+  
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const MAXIMUMTODOLIMIT = 10;
+
+  if(!user.pro && (user.todos.length >= MAXIMUMTODOLIMIT))
+    return response.status(403).json({ error: "You've reached the limit of TODOs for free account. Get premium to create more!" });
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user)
+    return response.status(404).json({ error: "User not found" });
+
+  const isUserIdValid = validate(id);  
+
+  if(!isUserIdValid)
+    return response.status(400).json({ error: "ID format not valid" });
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo)
+    return response.status(404).json({ error: "Todo not found"});
+    
+  request.todo = todo;
+  request.user = user;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  if(!id) {
+    return response.status(400).json({ error: 'ID is required' })
+  }
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({ error: "User not Found!"});
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
